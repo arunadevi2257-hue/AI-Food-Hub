ead538c7c4c39187e65da8a6f55703c68db0c157
arunadevi2257-hue <arunadevi2257@gmail.com>
2026-07-03 14:26:47 +0530

    Fix MySQL connection for Render

--- app.py
+++ app.py
@@ -1,58 +1,45 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
-from config import Config
+import os
+
 from routes.auth import auth, init_mysql
 from routes.menu import menu, init_menu
 from routes.cart import cart, init_cart
 from routes.order import order, init_order
 from routes.admin import admin, init_admin
-import os
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
+
+app.config["UPLOAD_FOLDER"] = "static/food_images"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default_secret")
+
 
 mysql = MySQL(app)
 
 init_mysql(mysql)
+init_menu(mysql)
+init_cart(mysql)
+init_order(mysql)
+init_admin(mysql)
+
 
 app.register_blueprint(auth)
-
-init_menu(mysql)
 app.register_blueprint(menu)
-
-init_cart(mysql)
 app.register_blueprint(cart)
-
-init_order(mysql)
 app.register_blueprint(order)
-
-init_admin(mysql)
 app.register_blueprint(admin)
 
-app.config['UPLOAD_FOLDER'] = 'static/food_images'
 
 @app.route("/")
 def home():
     return render_template("home.html")
-
-@app.route("/menu")
-def menu():
-    return render_template("menu.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
-
-
-@app.route("/cart")
-def cart():
-    return render_template("cart.html")
-
 
 @app.route("/test_db")
 def test_db():
@@ -63,6 +50,7 @@ def test_db():
         return "✅ Database Connected Successfully!"
     except Exception as e:
         return f"❌ Database Error: {e}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
